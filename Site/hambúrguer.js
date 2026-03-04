ce97805775409712b7177bd3a42c3216df4c0d33
capitu425 <tataiobuarque@gmail.com>
2026-03-04 20:58:53 -0300

    Add files via upload
    Esse código é melhor, é a versão que eu fiz em casa
    Diferenças: Design, Biografia, menu

---
+++ Site/hambúrguer.js
@@ -0,0 +1,6 @@
+const menuHamburguer = document.getElementById('menu-hamburguer');
+
+menuHamburguer.addEventListener('click', () => {
+    menuHamburguer.classList.toggle('active');
+    // Aqui você pode adicionar lógica para abrir o menu
+});
